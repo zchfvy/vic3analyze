ac005cb0158d333718b1942f2c3b6f85055b0f97
Jason Hamilton-Smith <hs.jason@gmail.com>
2022-11-04 00:10:11 -0700

    Fix bad savegame dir from last commit

--- vic3analyze/conf.py
+++ vic3analyze/conf.py
@@ -1,5 +1,4 @@
 import os
-from appdirs import user_data_dir
 
 _config = {}
 
@@ -7,15 +6,16 @@ from sys import platform
 if platform == "linux" or platform == "linux2":
     # linux
     game_dir = os.path.expanduser('~/.steam/steam/steamapps/common/Victoria 3')
+    save_dir = os.path.expanduser('~/.local/share/Paradox Interactive/Victoria 3/save games')
 elif platform == "darwin":
     # OS X
     # Just assumin it's the same as linux, don't know for sure
     game_dir = os.path.expanduser('~/.steam/steam/steamapps/common/Victoria 3')
+    save_dir = os.path.expanduser('~/.local/share/Paradox Interactive/Victoria 3/save games')
 elif platform == "win32":
     # Windows...
-    game_dir = r'C:\Program Files\Steam\steamapps\common\Victoria 3'
-
-saves_dir = os.path.join(user_data_dir('Paradox Interactive', 'Victoria 3'), 'save games')
+    game_dir = os.path.expandvars(r'%ProgramFiles%\Steam\steamapps\common\Victoria 3')
+    save_dir = os.path.expandvars(r'%HOMEPATH%\Documents\Paradox Interactive\Victoria 3\save games')
 
 _config['game_dir'] = game_dir
 _config['saves_dir'] = saves_dir
